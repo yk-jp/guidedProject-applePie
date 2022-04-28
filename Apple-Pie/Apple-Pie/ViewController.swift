b9286fe354391b34dcaa9e2d64ab647cac9a1d7b
yk-jp <k82.y12@gmail.com>
2022-04-28 00:33:28 -0700

    add image and buttons in a single view

---
+++ Apple-Pie/Apple-Pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple-Pie
+//
+//  Created by Yusuke K on 2022-04-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
